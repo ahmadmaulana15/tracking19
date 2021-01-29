7575bce9a539266aa84992a045cbbc1deeeea21d
jay150503maulana <fajarpro27@gmail.com>
2021-01-28 17:54:12 -0800

    merapihkan tampilan livewire

--- resources/views/kasus2/create.blade.php
+++ resources/views/kasus2/create.blade.php
@@ -1,58 +1,60 @@
 @extends('layouts.admin')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><center><b>{{ __('Data Kasus Local') }}</b></center></div>
-
-                <div class="card-body">
-                <form action="{{route('kasus2.store')}}" method="POST">
-                @csrf
-                    @if (session('status'))
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                @if (session('message'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            {{ session('message') }}
                         </div>
-                    @endif
-                    <div class="row">
+                @endif
+                <div class="card">
+                    <div class="card-header">
+                        <p>Tambah Data Kasus</p>
+                    </div>
+                    
+                    <div class="card-body">
+                        <form action="{{ route('kasus2.store') }}" method="POST">
+                        @csrf
                             <div class="col">
-                                @livewire('kasus1')
+                                <livewire:kasus1 />
                             </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Jumlah Positif</label>
-                        <input type="number" name="jumlah_positif" class="form-control" id="">
-                        @if($errors->has('jumlah_positif'))
-                        <span class="text-danger">{{ $errors->first('jumlah_positif')}}</span>
-                        @endif
+                            
+                                <center><h2><p>-- Data Kasus --</p></h2></center>
+                            
+                            <div class="form-group">
+                                <label for="">Jumlah Positif</label>
+                                <input type="text"name="jumlah_positif" class="form-control" id="exampleInputEmail1"  placeholder="Jumlah Positif">
+                                @if($errors->has('jumlah_positif'))
+                                    <span class="text-danger">{{ $errors->first('jumlah_positif') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="">Jumlah Meninggal</label>
+                                <input type="text" name="jumlah_meninggal" class="form-control" id="exampleInputEmail1"  placeholder="Jumlah Meninggal">
+                                @if($errors->has('jumlah_meninggal'))
+                                    <span class="text-danger">{{ $errors->first('jumlah_meninggal') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="">Jumlah Sembuh</label>
+                                <input type="text" name="jumlah_sembuh" class="form-control" id="exampleInputEmail1"  placeholder="Jumlah Sembuh">
+                                @if($errors->has('jumlah_sembuh'))
+                                    <span class="text-danger">{{ $errors->first('jumlah_sembuh') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="">Tanggal</label>
+                                <input type="date" name="tanggal" class="form-control" id="exampleInputEmail1"  placeholder="Jumlah Sembuh">
+                            </div>
+                            <a href="{{url()->previous()}}" class="btn btn-danger">Kembali</a>
+                            <div class="float-right">
+                                <button type="submit" class="btn btn-success">Submit</button>
+                            </div>
+                        </form>
                     </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Jumlah Meninggal</label>
-                        <input type="number" name="jumlah_meninggal" class="form-control" id="">
-                        @if($errors->has('jumlah_meninggal'))
-                        <span class="text-danger">{{ $errors->first('jumlah_meninggal')}}</span>
-                        @endif
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Jumlah Sembuh</label>
-                        <input type="number" name="jumlah_sembuh" class="form-control" id="">
-                        @if($errors->has('jumlah_sembuh'))
-                        <span class="text-danger">{{ $errors->first('jumlah_sembuh')}}</span>
-                        @endif
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Tanggal</label>
-                        <input type="date" name="tanggal" class="form-control" id="">
-                        @if($errors->has('tanggal'))
-                        <span class="text-danger">{{ $errors->first('tanggal')}}</span>
-                        @endif
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
